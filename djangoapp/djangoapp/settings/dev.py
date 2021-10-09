56d53e1c21a7367673185bfe66e1f0938d0aae18
Alexander Razzhivin <admin@docode.ru>
2021-10-10 01:10:45 +0300

    send notifiactions about subscribers to email or telegram

--- djangoapp/djangoapp/settings/dev.py
+++ djangoapp/djangoapp/settings/dev.py
@@ -7,3 +7,22 @@ SECRET_KEY = 'y4!2#=i&^4u46xla^a&edsog)52__vv!!((5jk#j7)q=0w=9*t'
 
 DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# Run server fake smpt server: python -m smtpd -n -c DebuggingServer localhost:1025
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+
+# -----------------------
+# Subs settings
+# -----------------------
+NOTIFS_MESSAGE_FUNC = lambda request: f'New subscriber {request.data.get("name")} from host {request.data.get("host")}.'
+
+NOTIFS_EMAIL_RECIPIENT_LIST = []
+
+# How to setup simple tg bot read:
+# https://medium.com/@ManHay_Hong/how-to-create-a-telegram-bot-and-send-messages-with-python-4cf314d9fa3e
+TELEGRAM_BOT_TOKEN = ''
+TELEGRAM_BOT_CHAT_ID = ''
--- djangoapp/subs/views.py
+++ djangoapp/subs/views.py
@@ -1,6 +1,44 @@
 from rest_framework import generics
+import requests
 from .serializers import SubscriberSerializer
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 class CreateSubscriberView(generics.CreateAPIView):
     serializer_class = SubscriberSerializer
+
+    def _send_telegram(self, message):
+        bot_token = getattr(settings, "TELEGRAM_BOT_TOKEN", '')
+        bot_chat_id = getattr(settings, "TELEGRAM_BOT_CHAT_ID", '')
+
+        if not (bot_token and bot_chat_id):
+            return
+
+        send_text = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={bot_chat_id}&parse_mode=Markdown&text={message}'
+
+        response = requests.get(send_text)
+        return response.json()
+
+
+    def _send_email(self, message):
+        recipient_list = getattr(settings, "NOTIFS_EMAIL_RECIPIENT_LIST", [])
+        if recipient_list:
+            send_mail(
+                        subject=f'New subscriber',
+                        message=message,
+                        from_email=None,
+                        recipient_list=recipient_list,
+                        html_message=message,
+                    )
+
+    def post(self, request, *args, **kwargs):
+        message_func = getattr(settings, "NOTIFS_MESSAGE_FUNC", None)
+        if message_func:
+            message = message_func(request)
+        else:
+            message = f'New subscriber {request.data.get("name")} from host {request.data.get("host")}.'
+        
+        self._send_email(message)
+        self._send_telegram(message)
+
+        return self.create(request, *args, **kwargs)
